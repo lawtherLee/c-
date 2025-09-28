8d5c9b6b8055eef33f91cf970cb1ab4865b830a4
leeSZ <108440505+lawtherLee@users.noreply.github.com>
2025-09-28 17:31:52 +0800

    加了点注释

--- const/main.cpp
+++ const/main.cpp
@@ -9,8 +9,8 @@ int main()
     int a = 3;
     const int a2 = a;
     int* a3 = &a;
-    const int* a4 = &a;
-    int* const a5 = &a;
+    const int* a4 = &a; //指向常量的指针 不能通过这个指针修改它所指向的数据。指针本身可以被重新赋值以指向其他变量
+    int* const a5 = &a; // 常量指针 初始化后不能改变其指向 可以通过这个指针修改它所指向的数据
 
     int const* const a6 = &a;
     const int* const a7 = &a;
