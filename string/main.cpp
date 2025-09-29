2501edefa2f009df4f0466cca5abbd0f827fe875
leeSZ <108440505+lawtherLee@users.noreply.github.com>
2025-09-29 17:30:39 +0800

    1

--- string/main.cpp
+++ string/main.cpp
@@ -1,14 +1,14 @@
 //
 // Created by 尚泽 on 2025/9/29.
 //
-#include<string.h>
+#include<string>
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    string str;
+    std::string str;
     if (str.empty())
         cout << "str is empty" << ",len= " << str.length() << endl;
     else
