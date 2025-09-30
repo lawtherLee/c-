0ff2f6b1821e446697b297ee3db90e1880af298a
leeSZ <108440505+lawtherLee@users.noreply.github.com>
2025-09-30 16:55:45 +0800

    字符串相关的操作

--- string/main.cpp
+++ string/main.cpp
@@ -13,5 +13,34 @@ int main()
         cout << "str is empty" << ",len= " << str.length() << endl;
     else
         cout << "str is not empty" << ",len= " << str.length() << endl;
+
+    str = str.append("abcdefg");
+    cout << "str is " << str << ", size=" << str.size() << endl;
+
+    const char* p = str.c_str();
+    cout << "p=" << p << endl;
+    cout << "find:de=" << str.find("de", 0) << endl;
+    cout << "find:de=" << str.find("de", 4) << endl;
+
+    std::string str1 = str.insert(4, "123");
+    cout << "str1=" << str1 << endl;
+
+
+    std::string s1 = "C++语言";
+    s1[0] = 'j';
+    cout << "s1=" << s1 << endl;
+
+    std::string s2 = s1.substr(3, 4);
+    char ctr[20];
+    strcpy(ctr, s2.c_str());
+    cout << "ctr=" << ctr << endl;
+    cout << "s1=" << s1 << ",s2=" << s2 << endl; // j++语言 语言
+    s1.swap(s2);
+    cout << "s1=" << s1 << ",s2=" << s2 << endl; // 语言 j++语言
+
+    cout << s2.find(ctr) << endl;
+
+
+    return 0;
 }
 
