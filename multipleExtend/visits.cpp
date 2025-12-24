88ae45d1a14a7ae242fdc8c0fb61b5ab06ce06ca
leeSZ <108440505+lawtherLee@users.noreply.github.com>
2025-12-24 17:30:41 +0800

    多重继承1.0

---
+++ multipleExtend/visits.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 尚泽 on 2025/12/24.
+//
+
+#include "visits.h"
---
+++ multipleExtend/visits.h
@@ -0,0 +1,14 @@
+//
+// Created by 尚泽 on 2025/12/24.
+//
+
+#ifndef C___VISITS_H
+#define C___VISITS_H
+
+
+class CB1
+{
+};
+
+
+#endif //C___VISITS_H
