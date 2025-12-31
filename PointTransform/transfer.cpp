cb4d1cd64ed03f5b80afa8b3ea815affb243ef73
leeSZ <108440505+lawtherLee@users.noreply.github.com>
2025-12-31 11:17:05 +0800

    指针在类型之间的转换

---
+++ PointTransform/transfer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 尚泽 on 2025/12/30.
+//
+
+#include "transfer.h"
---
+++ PointTransform/transfer.h
@@ -0,0 +1,14 @@
+//
+// Created by 尚泽 on 2025/12/30.
+//
+
+#ifndef C___TRANSFER_H
+#define C___TRANSFER_H
+
+
+class transfer
+{
+};
+
+
+#endif //C___TRANSFER_H
