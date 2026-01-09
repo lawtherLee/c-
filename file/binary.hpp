b8ad6e7158318b531eb98d53edfc29bd3010c427
leeSZ <108440505+lawtherLee@users.noreply.github.com>
2026-01-09 17:53:17 +0800

    读写文件对指针的一些操作

--- file/binary.hpp
+++ file/binary.hpp
@@ -1,5 +1,6 @@
 #ifndef BINARY_HPP
 #define BINARY_HPP
+#include <climits>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
@@ -41,6 +42,10 @@ inline void readBinaryStudent() {
   if (!inFile) {
     cout << "open student.dat error!" << endl;
   }
+
+  cout << "打开文件时位置指针：" << inFile.tellg() << endl;
+  cout << "每个记录大小：" << sizeof(CStudent) << endl;
+
   CStudent stu;
   int count{}, nbyte{};
   cout << "学号 姓名 成绩\n";
@@ -50,7 +55,46 @@ inline void readBinaryStudent() {
     count++;
     nbyte += inFile.gcount();
   }
+
+  cout << "读取文件结束时指针位置：" << inFile.tellg() << endl;
+  inFile.clear(); // 清除流状态标志
+  inFile.seekg(0);
+  cout << "文件指针已重置到文件开头" << endl;
+  // 需要重新read 否则stu保存的最后一行数据 重置指针后在读取还是最后一行
+  inFile.read((char *)&stu, sizeof(stu));
+  cout << left << setw(10) << stu.id << " " << setw(20) << stu.name << " "
+       << setw(3) << right << stu.score << endl;
+  cout << "指针位置：" << inFile.tellg() << endl;
+  inFile.seekg(0, ios::end);
   cout << "共读取 " << count << " 条记录，占用 " << nbyte << " 字节" << endl;
+
+  // 查找最高分和最低分
+  inFile.clear();
+  inFile.seekg(0);
+  count = 0;
+
+  int maxScore = INT_MIN, minScore = INT_MAX, maxNo{}, minNo{};
+  while (inFile.read(((char *)&stu), sizeof(stu))) {
+    if (stu.score > maxScore) {
+      maxScore = stu.score;
+      maxNo = count;
+    } else if (stu.score < minScore) {
+      minScore = stu.score;
+      minNo = count;
+    }
+    count++;
+  }
+
+  inFile.clear();
+  inFile.seekg(sizeof(stu) * maxNo,
+               ios::beg);                 // 二进制文件随机访问的核心技巧
+  inFile.read((char *)&stu, sizeof(stu)); // 读最高分
+  cout << "最高分：" << maxNo << ": " << stu.id << " " << stu.name << " "
+       << stu.score << endl;
+  inFile.seekg(sizeof(stu) * minNo, ios::beg); // 读最低分
+  inFile.read((char *)&stu, sizeof(stu));
+  cout << "最低分：" << minNo << ": " << stu.id << " " << stu.name << " "
+       << stu.score << endl;
   inFile.close();
 }
 #endif
